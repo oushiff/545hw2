b082187a37090dea26d0cecdd38d108e266a0660
fanf <fanf@oupeng.com>
2017-04-02 14:08:54 -0700

    f

--- my_inverse.m
+++ my_inverse.m
@@ -1,5 +1,6 @@
-aaaaa = inverse_kinematics([1 1 1 2 2 1 1 1]);
-aaaaa;
+
+aaaaa = inverse_kinematics([1 1 1 1 2 2 1 1  1 1]);
+aaaaa
 
 function thetad = inverse_kinematics(u)
     % performs inverse kinematics computations with various methods
@@ -23,8 +24,7 @@ function thetad = inverse_kinematics(u)
     matrix_sum = [];
     
     x_array = [];
-    y_array = [];
-    
+    y_array = []; 
     x_array_sum = [];
     y_array_sum = [];
     
@@ -35,46 +35,28 @@ function thetad = inverse_kinematics(u)
         theta_sum = theta_sum + theta(index);
         line = links(index);
         delta_x = -line * sin(theta_sum);
-        x_array = [x_array [0; 0; 0]];
+        x_array = [x_array [0]];
         x_array = x_array + delta_x;
-        x_array_sum = [x_array_sum [0; 0; 0]];
+        x_array_sum = [x_array_sum [0]];
         x_array_sum = x_array_sum + x_array;
         
         delta_y = line * cos(theta_sum);
-        y_array = [y_array [0; 0; 0]];
+        y_array = [y_array [0]];
         y_array = y_array + delta_y;
-        y_array_sum = [y_array_sum [0; 0; 0]];
+        y_array_sum = [y_array_sum [0]];
         y_array_sum = y_array_sum + y_array;
-        index = index + 1;
+        index = index + 1;     
     end 
     
-    J = (1/n) * [x_array_sum; y_array_sum; zeros(n)];
-
+    J = (1/n) * [x_array_sum; y_array_sum; zeros(1, n)];
     % convert cartesian velocities into joint velocities
 
     % NOTE: insert the required inverse kinematics methods at this
     %       location
    
-    
-    % thetad = alpha * transpose(J) * [xd(1); xd(2); 0];
-    thetad = 1;
+    thetad = alpha * mtimes(transpose(J) , [xd(1); xd(2); 0]);
 end
 
-
-
-
-function former_jt = j_transpose(former_jt, line, theta_sum)
-    former_jt = vertcat(former_jt, [0 0 0]);
-    delta_1 = -line * sin(theta_sum);
-    delta_2 = line * cos(theta_sum);
-    index = 1;
-    len = size(former_jt);
-    while index <= len
-        former_jt(index, 1) = former_jt(index, 1) + delta_1;
-        former_jt(index, 2) = former_jt(index, 2) + delta_2;
-        index = index + 1;
-    end
-end
 
 
 
